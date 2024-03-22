e0d462506026dfe9ad1e3df065b4079e61f3c4ad
Falk Werner <falk.werner@gmx.net>
2024-03-22 20:00:02 +0100

    remove unused C++ code

--- src/pe_depends_static/dll_imports.c
+++
@@ -1,6 +0,0 @@
-#include "dll_imports.h"
-
-struct str_array * dll_imports(char const * filename)
-{
-    return 0;
-}
--- src/pe_depends_static/status.h
+++
@@ -1,9 +0,0 @@
-#ifndef PE_DEPENDS_STATUS_H
-#define PE_DEPENDS_STATUS_H
-
-enum status {
-    GOOD,
-    BAD
-};
-
-#endif
